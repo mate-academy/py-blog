47a7c89e0b99b785e47afec7ca05ddfe117bbbbf
IvanGLS <gladiolus0707@gmail.com>
2022-12-12 23:16:13 +0200

    'Solution'

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(CommentaryCreateView, self).get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Post, pk=self.kwargs["pk"])
+        context["post"] = get_object_or_404(Post, pk=self.kwargs["pk"])
         return context
 
     def form_valid(self, form):
