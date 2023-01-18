0ae2bbbe90453d1921840cd51c383354165c3b90
Taras Martyniuk <bythewaters800@gmail.com>
2023-01-18 16:41:42 +0100

    Blog

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from blog.views import (
+    PostListView,
+    post_detail_view,
+)
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="index"),
+    path(
+        "post/<int:pk>/",
+        post_detail_view,
+        name="post-detail"
+    ),
+]
+
+app_name = "blog"
