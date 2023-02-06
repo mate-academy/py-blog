10f0a01279c66ce5513b59cda1908bac91d319b4
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-02-06 19:17:52 +0800

    Draft 3.0\

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,11 @@ from blog.views import PostListView, PostDetailView, CommentaryCreateView
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/create/", CommentaryCreateView.as_view(), name="post-detail"),
+    path(
+        "posts/<int:pk>/add_comment/",
+        CommentaryCreateView.as_view(),
+        name="post-add-comment"
+    ),
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
+from blog.forms import CommentForm
 from blog.models import Post, Commentary
 
 
@@ -11,14 +14,35 @@ class PostListView(generic.ListView):
     template_name = "blog/index.html"
 
 
+class CommentaryCreateView(generic.CreateView):
+    # model = Commentary
+    # fields = "__all__"
+    # template_name = "blog/commentary_form.html"
+    # success_url = reverse_lazy("blog:post-detail")
+    def post(self, request, *args, **kwargs):
+        post_id = kwargs["pk"]
+        post_url = reverse("blog:post-detail", kwargs={"pk": post_id})
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            content = form.cleaned_data["content"]
+
+            if post_id and content:
+                form.instance.user_id = self.request.user.pk
+                form.instance.post_id = post_id
+                self.success_url = post_url
+                return super().form_valid(form)
+
+        return HttpResponseRedirect(post_url)
+
+
 class PostDetailView(generic.DetailView):
     model = Post
     queryset = Post.objects.select_related("owner").prefetch_related("commentaries__user")
     template_name = "blog/post_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
 
-class CommentaryCreateView(generic.CreateView):
-    model = Commentary
-    fields = "__all__"
-    template_name = "blog/commentary_form.html"
-    success_url = reverse_lazy("blog:post-detail")
