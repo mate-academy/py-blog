141db9177ec52db68809ac2123e511a940cfa982
Ihor Ivashov <dm-i@i.ua>
2023-02-16 17:23:20 +0200

    Fixed flake8 errors

--- blog/models.py
+++ blog/models.py
@@ -24,8 +24,16 @@ class Post(models.Model):
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentaries")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commentaries")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
     content = models.CharField(max_length=255)
     created_time = models.DateTimeField(auto_now_add=True)
 
--- blog_system/settings.py
+++ blog_system/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e58!h%w8sr1t7_*0yqe)7f9cg29_n9%s*qu$s_3qz=%%$gmx_d"
+SECRET_KEY = \
+    "django-insecure-e58!h%w8sr1t7_*0yqe)7f9cg29_n9%s*qu$s_3qz=%%$gmx_d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,16 +95,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".NumericPasswordValidator",
     },
 ]
 
