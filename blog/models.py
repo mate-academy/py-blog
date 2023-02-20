b6dc7393992596f1063783fbb8a618c3ebeccef7
Anna Kabatova <kabatova.anna@ukr.net>
2023-02-20 18:15:18 +0200

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name="comments")
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        related_name="comments"
+        )
+    created_time = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
