c45346ee2234bbf0574ea997d241b3131fa4a24e
ViktorKr <kravchukviktors@gmail.com>
2023-02-26 12:06:08 +0200

    blog

---
+++ blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from blog.models import Post, Commentary, User
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["created_time", "content", "owner"]
+    search_fields = ("title", "content")
+    list_filter = ("title", "owner")
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ["pk", "created_time", "content", "post"]
+    search_fields = ("content",)
+    list_filter = ("post",)
