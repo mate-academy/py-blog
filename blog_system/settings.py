0ca581c912b49936b2364d62c5f88e0b5a28518b
Piter Abramov <sebshe3@gmail.com>
2023-03-08 20:25:40 +0200

    change_fields_and_settings_6

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -10,12 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = (
+    "django-insecure-ff8d_juj^xv!^*49_yc-jifeos!xt7b_ku@l+8@3u(-vp*)i=+"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
