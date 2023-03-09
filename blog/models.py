2b767a527cdca10c05d76d2904b3c089935ec5f6
Roman Pysarskiy <113036822+Soobig666@users.noreply.github.com>
2023-03-09 15:55:47 +0200

    Start project

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.
