b95fc2e0f9db9861977a46aedaf46c9bab29061c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-19 17:59:27 +0200

    Solution

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap4",
+    # "bootstrap4",
     "crispy_bootstrap4",
     "crispy_forms",
     "debug_toolbar",
