34b56831ed218c5cab397554c00fc9e542147d71
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-19 17:50:55 +0200

    Solution

---
+++ blog/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+
+class User(AbstractUser):
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post-detail", kwargs={"pk": self.pk})
+
+    class Meta:
+        ordering = ["-created_time"]
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "commentaries"
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return f"{self.user} {self.post}"
---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
+
+from blog.forms import CommentaryCreateForm
+from blog.models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    paginate_by = 5
+    queryset = Post.objects.all().select_related("owner")
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    context_object_name = "post_detail"
+    queryset = Post.objects.all().select_related("owner")
+
+
+class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
+    def post(self, request, *args, **kwargs):
+        post_id = kwargs["pk"]
+        post_url = reverse_lazy("blog:post-detail", kwargs={"pk": post_id})
+        form = CommentaryCreateForm(request.POST)
+
+        if form.is_valid():
+            content = form.cleaned_data["content"]
+
+            if post_id and content:
+                form.instance.user_id = self.request.user.pk
+                form.instance.post_id = post_id
+                self.success_url = post_url
+                return super().form_valid(form)
+
+            return HttpResponseRedirect(post_url)
