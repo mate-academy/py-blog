fb4572b04885e3ffcd14dea5583e1d7d7d3e5a03
Haishuk Pavlo <xomas@ukr.net>
2023-03-23 02:26:50 +0200

    main page buttons & commentary page configured

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, CommentaryListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("commentary/<int:pk>", CommentaryListView.as_view(), name="commentary")
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-from blog.models import Post
+from blog.models import Post, Commentary
 
 
 def index(request):
@@ -22,3 +22,8 @@ def index(request):
     }
 
     return render(request, "index.html", context=context)
+
+
+class CommentaryListView(generic.ListView):
+    model = Commentary
+    template_name = "commentary.html"
