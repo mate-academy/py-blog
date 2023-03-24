52c9fdee779ee04ae8cb4204bf7ea031ae451d0a
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-03-24 16:30:59 +0100

    login Solution

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,11 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="posts",
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_created=True)
@@ -24,8 +28,16 @@ class Post(models.Model):
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="commentaries", on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name="commentaries", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="commentaries",
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post,
+        related_name="commentaries",
+        on_delete=models.CASCADE
+    )
     created_time = models.DateTimeField(auto_now_add=True, blank=True)
     content = models.TextField()
 
@@ -33,4 +45,4 @@ class Commentary(models.Model):
         verbose_name_plural = "commentaries"
 
     def __str__(self) -> str:
-        return f"{self.user.username}: \"{self.content}\""
+        return f'{self.user.username}: "{self.content}"'
