e0e02b23e1b7423396e166e60885be369177bfad
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-24 13:46:58 +0200

    Solution

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views import generic
+
+from blog.models import Post, Commentary
+
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by("-created_time")
+    template_name = "blog/index.html"
+    context_object_name = "post_list"
+    paginate_by = 5
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        content = request.POST.get("content")
+        if content:
+            Commentary.objects.create(
+                user=request.user,
+                post=post,
+                content=content
+            )
+            return redirect("blog:post-detail", pk=post.pk)
+        raise ValidationError("Empty comment is restricted!")
