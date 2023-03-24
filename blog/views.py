cd66fb3298a44c048e756c1a4c311259672fe002
Alina Boichenko <ilub1108@gmail.com>
2023-03-24 17:20:28 +0200

    deleted is_authenticated

--- blog/views.py
+++ blog/views.py
@@ -22,18 +22,17 @@ class PostDetailView(DetailView):
 
 class CommentaryCreateView(LoginRequiredMixin, CreateView):
     def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            post_id = kwargs["pk"]
-            post_url = reverse_lazy("blog:post_detail", kwargs={"pk": post_id})
-            form = CommentaryCreateForm(request.POST)
+        post_id = kwargs["pk"]
+        post_url = reverse_lazy("blog:post_detail", kwargs={"pk": post_id})
+        form = CommentaryCreateForm(request.POST)
 
-            if form.is_valid():
-                content = form.cleaned_data["content"]
+        if form.is_valid():
+            content = form.cleaned_data["content"]
 
-                if post_id and content:
-                    form.instance.user_id = self.request.user.pk
-                    form.instance.post_id = post_id
-                    self.success_url = post_url
-                    return super().form_valid(form)
+            if post_id and content:
+                form.instance.user_id = self.request.user.pk
+                form.instance.post_id = post_id
+                self.success_url = post_url
+                return super().form_valid(form)
 
-            return HttpResponseRedirect(post_url)
+        return HttpResponseRedirect(post_url)
