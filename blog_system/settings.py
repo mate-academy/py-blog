aea5fb7f65f6b9d0c8945d95ea962801c9de7235
Natalie Goriela <natalie.goriela@gmail.com>
2023-03-28 16:41:55 +0300

    'Solution'

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
     "crispy_forms",
     "blog",
 ]
@@ -136,3 +137,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
