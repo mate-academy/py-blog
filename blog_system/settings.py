cfaa9fc1883f26c8db425e4ce5adb95d7904918e
Oleksandr Mamchur <irex1988@gmail.com>
2023-04-05 01:07:30 +0300

    removed_secret_key

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -20,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    "django-insecure-20dy9@fafj^3e)s$6yypu6-y7ghxr!w2-zz*0#eoude7-)cjv6"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
