3703a94fb6dd3ebbbf421242ab56e1fcec8e7c57
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-04-09 20:01:51 +0300

    solution

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.urls import reverse
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" Post {self.title} of user {self.owner}"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "comments"
