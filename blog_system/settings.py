cee23e420ac4ac146cae339182ddb11aa4e6ef14
Illia Smyrnov <ilushasm@gmail.com>
2023-04-18 21:15:14 +0300

    Solution settings.py flake8

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -103,7 +103,10 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME":
-            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+            (
+                "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator"
+            ),
     },
     {
         "NAME":
