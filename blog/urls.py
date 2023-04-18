f4dc12d10a5b69855df67d2ab3cd345a2908ed09
Illia Smyrnov <ilushasm@gmail.com>
2023-04-18 03:45:53 +0300

    Solution

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from blog.forms import CommentaryForm
+from blog.views import (
+    PostListView,
+    PostDetailView
+)
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="index"),
+    path(
+        "posts/<int:pk>/",
+        PostDetailView.as_view(),
+        name="post-detail"
+    ),
+    path(
+        "posts/comment/",
+        CommentaryForm,
+        name="post-comment"
+    )
+]
+
+app_name = "blog"
