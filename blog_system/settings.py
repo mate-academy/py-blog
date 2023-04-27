2a7c42855c87baa6054df642c1784d089ad88597
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-04-27 22:27:45 +0300

    removed some from settings.py

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-59q!c(#g$1$txg#gstr6z-gzyf0gma+531d-75vrn6dlj90)v3"
+SECRET_KEY = (
+    "django-insecure-59q!c(#g$1$txg#gstr6z-gzyf0gma+531d-75vrn6dlj90)v3"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,20 +92,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
 
 AUTH_USER_MODEL = "blog.User"
 
