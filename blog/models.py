89be98691292e3cefbdc808334574a1948a58b2a
Andriy <an_mydel@gmail.com>
2023-05-13 22:22:39 +0300

    Solution

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentaries")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commentaries")
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name = "Commentary"
+        verbose_name_plural = "Commentaries"
