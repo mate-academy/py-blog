0c4d578f9d67ec28a50e43f6424fd388b7a1e8d8
Tetiana Kozakova <kleotan901@gmail.com>
2023-05-14 18:27:09 +0200

    solution

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from blog.views import PostList, post_detail
+
+urlpatterns = [
+    path("", PostList.as_view(), name="index"),
+    path("posts/<int:pk>", post_detail, name="post-detail"),
+    path("posts/<int:pk>/comment/create", post_detail, name="comment-create"),
+]
+
+app_name = "blog"
---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.views import generic
+
+from blog.forms import CommentForm
+from blog.models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    template_name = "blog/index.html"
+    paginate_by = 5
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = CommentForm(request.POST or None)
+    context = {
+        "post": post,
+        "form": CommentForm(initial={"post": post, "user": request.user})
+    }
+    if request.user.is_authenticated:
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.save()
+            return redirect("blog:post-detail", pk=post.pk)
+    else:
+        form.fields["content"].widget.attrs["readonly"] = True
+
+    return render(request, "blog/post_detail.html", context)
