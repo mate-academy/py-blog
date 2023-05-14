cfc178d445ff6b6fec597258f26772c64a686009
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-05-14 14:09:10 +0300

    Solution5

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -8,19 +9,19 @@ from blog.forms import CommentaryCreateForm
 from blog.models import Commentary, Post
 
 
-# def index(request):
-#     post_list = Post.objects.all()
-#     num_commentary = Commentary.objects.count()
-#     num_visits = request.session.get("num_visits", 0)
-#     request.session["num_visits"] = num_visits + 1
-#
-#     context = {
-#         "post_list": post_list,
-#         "num_commentary": num_commentary,
-#         "num_visits": num_visits + 1,
-#     }
-#
-#     return render(request, "blog/index.html", context=context)
+def index(request):
+    post_list = Post.objects.all()
+    num_commentary = Commentary.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "post_list": post_list,
+        "num_commentary": num_commentary,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "blog/index.html", context=context)
 
 
 class PostDetailView(generic.DetailView):
