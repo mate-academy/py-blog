bf5d4689ed107a49a64136ecd5cb8fd1f544d0c9
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-05-18 12:49:31 +0300

    With_fixed_path

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from blog.views import IndexListView, PostDetailView
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
-    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
