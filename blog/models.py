2e3c4063846cd02eea34843a88c8d49a83cb5873
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-20 17:49:44 +0300

    Created models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    users = models.ForeignKey("User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Commentary(models.Model):
+    users = models.ForeignKey("User", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
