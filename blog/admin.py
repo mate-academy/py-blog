079df0f81be557a80c7a39aebc645ef5f6c32a73
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-22 22:47:34 +0300

    Registered all model & unregistered Group in the admin.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+from blog.models import Post, Commentary, User
+
 admin.site.unregister(Group)
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Commentary)
