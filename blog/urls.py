bf2a48f8ee80cb0a26e4159d88d0180758258ab6
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-06-01 14:14:46 +0300

    'Solution'

---
+++ blog/urls.py
@@ -0,0 +1,22 @@
+"""blog URL Configuration
+"""
+
+from django.urls import path
+
+from blog.views import PostListView, PostDetailView, CommentaryCreateView
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="index"),
+    path(
+        "posts/<int:pk>/",
+        PostDetailView.as_view(),
+        name="post-detail"
+    ),
+    path(
+        "posts/<int:pk>/create_comment/",
+        CommentaryCreateView.as_view(),
+        name="commentary-create"
+    ),
+]
+
+app_name = "blog"
