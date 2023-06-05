8becc71b8e51bbf587733c94e4f5b4f861646582
Olena Liubynetska <olena.liuby@gmail.com>
2023-06-05 18:30:58 +0100

    Solution

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+
+from blog.models import Commentary, Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.select_related("owner")
+    fields = "__all__"
+    template_name = "blog/index.html"
+    success_url = reverse_lazy("blog:post-list")
+    paginate_by = 5
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    queryset = Post.objects.prefetch_related("commentaries")
+
+
+class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Commentary
+    fields = ["content"]
+    template_name = "blog/commentary_form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post = Post.objects.get(id=self.kwargs.get("pk"))
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post_id = self.kwargs["pk"]
+        return reverse_lazy("blog:post-detail", kwargs={"pk": post_id})
