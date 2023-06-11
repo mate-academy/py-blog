62368aab84580124c5b57c6b624e4949933f605c
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-11 22:18:36 +0300

    Finish!

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from blog.views import PostListView, PostDetailView
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    # path("post/<int:pk>/comment/", add_comment, name="add-comment")
 
 ]
 
