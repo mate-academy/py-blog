ce309777d82d9d82b2882612791573905b0551a5
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-17 13:06:26 +0300

    fix/use reverse_lazy

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path(
-        "posts/<int:pk>/add_commentary",
+        "posts/<int:pk>/add_commentary/",
         CommentaryCreateView.as_view(),
         name="add-commentary"
     )
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from typing import Callable
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
 
 from blog.models import Post, Commentary
@@ -22,7 +21,7 @@ class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Commentary
     template_name = "blog/add_commentary.html"
     fields = ["content"]
-    success_url = "/"
+    success_url = reverse_lazy(f"blog:index")
 
     def form_valid(self, form):
         post = get_object_or_404(Post, pk=self.kwargs["pk"])
