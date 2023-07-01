b7bb9081b0be39191bc2133c55b36091a79e6fc6
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-07-01 08:30:42 +0300

    Create models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["username"]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    class Meta:
+        ordering = ["created_time"]
+
+    def __str__(self):
+        return self.content
