ac800426449f15182d085082d2bcd1d0c7922f6f
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-07-01 09:07:25 +0300

    Register all models in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from blog.models import Post, Commentary, User
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.register(Post)
+admin.site.register(Commentary)
