304182d9c57d2aeb27acbd8ce6c326f24dac92aa
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-07-03 12:27:22 +0300

    Implement PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+
+from blog.models import Post
+
 
 # Create your views here.
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    ordering = ["-created_time"]
