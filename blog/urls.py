acd873a442b15870fe3ceb3912f1d34ff84a3672
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-07-03 13:47:29 +0300

    Fix posts/ url

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("posts", PostListView.as_view(), name="index"),
+    path("posts/", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
