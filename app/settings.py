1aab8b78828b7dd06aa17b658cd1bd23f56a1ed7
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-07-04 21:23:09 +0300

    Solution

--- app/settings.py
+++ app/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
-            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+            "django.contrib.auth.password_validation.\
+            UserAttributeSimilarityValidator"
         ),
     },
     {
