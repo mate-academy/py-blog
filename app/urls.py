49c6e934ccd599d92174649227038df827a82701
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-07-04 21:21:09 +0300

    Solution

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
     path("accounts/", include("django.contrib.auth.urls")),
 
