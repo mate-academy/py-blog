f35fbf710f43076b6f25d1ce78d20aa5cc69a49a
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-07-04 09:46:01 +0300

    Implement CommentaryForm in PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
+from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic.edit import FormMixin
 
+from blog.forms import CommentaryForm
 from blog.models import Post
 
 
-# Create your views here.
 class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.select_related("owner")
@@ -11,6 +13,34 @@ class PostListView(generic.ListView):
     paginate_by = 5
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(FormMixin, generic.DetailView):
     model = Post
     queryset = Post.objects.select_related("owner")
+    form_class = CommentaryForm
+
+    def get_success_url(self):
+        return reverse_lazy("blog:post-detail", kwargs={"pk": self.object.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context["form"] = CommentaryForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        post = self.get_object()
+        user = request.user
+
+        saved_form = form.save(commit=False)
+        saved_form.post = post
+        saved_form.user = user
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.save()
+        return super(PostDetailView, self).form_valid(form)
