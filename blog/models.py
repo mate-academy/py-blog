b6657e62332c33b61a879b373f838434bedcb1b2
Taras Savchyn <savik1992@gmail.com>
2023-07-12 00:09:32 +0300

    create models Blog, Post, Commentary

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=63)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=63)
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=63)
