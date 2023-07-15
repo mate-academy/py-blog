1c4fe48bc43e8d676608b57133b07d895bf49082
Oleksii Kolii <igravkompik@gmail.com>
2023-07-16 00:23:48 +0300

    solution

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import Textarea
+from django.utils.translation import gettext_lazy
+
+from blog.models import Commentary
+
+
+class CommentaryForm(forms.ModelForm):
+    class Meta:
+        model = Commentary
+        fields = ["content"]
+        labels = {
+            "content": gettext_lazy("Commentary")
+        }
