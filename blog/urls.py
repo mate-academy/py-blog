942c749b1016e408562f55924797c2e59ef402c8
Taras Savchyn <savik1992@gmail.com>
2023-07-18 23:24:33 +0300

    create post-list & post-dedail view and templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
-from .views import (index)
+from .views import (index, PostListView, PostDetailView)
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", PostListView.as_view(), name="index"),
+
+    path("posts/", PostListView.as_view(), name="post-list"),
+
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    # path("posts/create/", PostCreateView.as_view(), name="post-create"),
+    # path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    # path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+
+    # path("commentaries/", CommentaryListView.as_view(), name="commentary-list"),
+    # path("commentaries/<int:pk>/", CommentaryDetailView.as_view(), name="commentary-detail"),
+    # path("commentaries/create/", CommentaryCreateView.as_view(), name="commentary-create"),
+    # path("commentaries/<int:pk>/update/", CommentaryUpdateView.as_view(), name="commentary-update"),
+    # path("commentaries/<int:pk>/delete/", CommentaryDeleteView.as_view(), name="commentary-delete"),
 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
+from django.views import generic
 from django.shortcuts import render
+
+from blog.models import Post
+
 
 def index(request):
     return render(request, "blog/index.html")
+
+
+class PostListView(generic.ListView):
+    model = Post
+    paginate_by = 5
+
+class PostDetailView(generic.DetailView):
+    model = Post
