667556abd10b04ba445f91e1bf730071d5b25da2
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-07-19 13:35:54 +0300

    Add to slash "/"

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from blog.views import Index, PostDetailView, CommentCreateView
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
-    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path(
-        "posts/<int:pk>/create_comment",
+        "posts/<int:pk>/create_comment/",
         CommentCreateView.as_view(),
         name="post-create-comment"
     ),
