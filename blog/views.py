69a7e0e8991ff0e8a39312342dee3338b7b084fd
Taras Savchyn <savik1992@gmail.com>
2023-07-25 00:33:43 +0300

    add form to comment

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,35 @@
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import  redirect
+from django.urls import reverse
 
-from blog.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
 
 
-def index(request):
-    return render(request, "blog/index.html")
-
-
-class PostListView(generic.ListView):
+class PostListView(LoginRequiredMixin, generic.ListView):
     model = Post
     paginate_by = 5
 
-class PostDetailView(generic.DetailView):
+
+class PostDetailView(LoginRequiredMixin, generic.DetailView):
     model = Post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context["form"] = CommentaryForm()
+        return context
+
+
+def commentary_create(request):
+    form = CommentaryForm(request.POST or None)
+
+    if form.is_valid():
+        commentary = Commentary()
+
+        commentary.user = request.user
+        commentary.content = request.POST["content"]
+        commentary.post = Post.objects.get(title=request.POST["post"])
+        commentary.save()
+        return redirect(reverse("blog:post-list"))
