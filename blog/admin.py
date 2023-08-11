0f680ab126f154d25b6b143edc5e150388c07d11
neostyle88 <makssimm1@gmail.com>
2023-08-11 14:55:09 +0200

    reister models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from blog.models import User, Post, Commentary
+
+admin.site.unregister(Group)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Commentary)
--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,18 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
