5492eac63a3fcf6efd1fcbcf72b58d16312ed718
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-08-26 19:35:21 +0300

    Register admin models (+ flake8 corrections)

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from blog.models import Post, Commentary
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_filter = ("owner", "created_time",)
+    ordering = ("-created_time",)
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+    list_filter = ("user", "post",)
+    ordering = ("-crated_time",)
+
+
+admin.site.unregister(Group)
