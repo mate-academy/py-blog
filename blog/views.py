78c4267996845a4c0ca645f7d1456e80b9a43874
Vlad Kleshko <vkoesko@gmail.com>
2023-09-04 21:30:07 +0300

    i fixed mistakes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import FormMixin
