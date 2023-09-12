44eeb1c9b6e8213fcbbd30b3bb7501ccddd66f4b
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-12 21:58:58 +0200

    Set up home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from django.views import generic
 
-# Create your views here.
+from blog.models import Post, Commentary
+
+
+class IndexView(generic.ListView):
+    model = Post
+    queryset = (
+        Post.objects.order_by("-created_time").select_related("owner")
+        .prefetch_related(
+            Prefetch(
+                "commentaries",
+                queryset=Commentary.objects.select_related("user"),
+                to_attr="total_commentaries"
+            )
+        )
+    )
+    template_name = "blog/index.html"
+    paginate_by = 5
--- blog_system/settings.py
+++ blog_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
     "blog",
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = "blog_system.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -72,6 +73,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "blog_system.wsgi.application"
 
@@ -123,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
