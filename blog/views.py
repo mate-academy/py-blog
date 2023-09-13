801422948c19457c33f88753ed02951249d30f54
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-13 20:44:37 +0200

    Set up commentary system

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.db.models import Prefetch
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
+from blog.forms import CommentaryForm
 from blog.models import Post, Commentary
 
 
@@ -22,3 +25,19 @@ class IndexView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = Post
+    queryset = Post.objects.prefetch_related("commentaries", "commentaries__user")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["commentary_form"] = CommentaryForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        new_commentary = Commentary(
+            user=request.user,
+            post=post,
+            content=request.POST.get("content")
+        )
+        new_commentary.save()
+        return redirect(reverse_lazy("blog:post-detail", args=[post.pk]))
