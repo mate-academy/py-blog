8ba461eb08bdeb331a209641bc4484f800ea0d34
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-09-16 18:08:00 +0300

    Flake8 check

--- blog/views.py
+++ blog/views.py
@@ -63,6 +63,3 @@ class PostCreateView(LoginRequiredMixin, generic.CreateView):
         self.object.owner = self.request.user
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
-
-
-
