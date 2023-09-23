48adc226f4538a365c4fbaa2554e4a12c1500ca3
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-09-23 14:24:40 +0300

    creating an app blog

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    class Meta:
+        ordering = ("username",)
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ["-created_time"]
+        verbose_name = "commentary"
+        verbose_name_plural = "commentaries"
+
+    def __str__(self):
+        return f"{self.user} commented on {self.post}"
