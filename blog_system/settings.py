b390507ed5f5345a41fbfec08158c850d7048a14
OlegatorLE <oleh.travin@gmail.com>
2023-09-25 19:42:33 +0300

    added # noqa:E501

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -19,10 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure--bu-3p!vw*a7ono)8u&#67tqz9m&9xyyp_9jyt*qtcr7d@yxw&"  # noqa:E501
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
