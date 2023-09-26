e9e92fe3c49e4c3e23aa989ec505d674b5604bf7
Halyna Petrova <1208petrova@gmail.com>
2023-09-26 22:45:01 +0300

    'Solution7'

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
+    "crispy_bootstrap4",
     "blog",
 ]
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
