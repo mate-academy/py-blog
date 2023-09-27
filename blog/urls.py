e7bcc0e77610b5b99c2c3dd61bde9b0d6f464e97
laskarj <koks.adekvat@gmail.com>
2023-09-27 18:28:40 +0200

    Fixed.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import PostDetailView, index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
