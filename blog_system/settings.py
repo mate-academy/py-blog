f722c48c6fd01e47f4015b5757ac7e8180877b84
laskarj <koks.adekvat@gmail.com>
2023-09-27 17:52:17 +0200

    DjDT is disconnected

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -29,12 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
-
 
 # Application definition
 
@@ -45,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "blog",
     "crispy_forms",
     "crispy_bootstrap4",
@@ -59,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "blog_system.urls"
