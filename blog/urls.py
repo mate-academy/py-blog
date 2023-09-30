db55bc518004d22a8b0dcdab1623be6c30f5b925
Vasya Turok <turokvasil0@gmail.com>
2023-09-30 18:55:09 +0300

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from .views import (
     Index,
     PostDetailView,
     add_comment,
+    PostCreateView,
+
 )
 
 
@@ -11,7 +13,7 @@ urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("add_comment/<int:pk>/", add_comment, name="add-comment"),
-
+    path("post/create/", PostCreateView.as_view(), name="post-create"),
 ]
 
 app_name = "taxi"
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
 from blog.models import Post, Commentary
@@ -23,3 +26,15 @@ def add_comment(request, pk) -> None:
             comment = Commentary(user=request.user, post=post, content=content)
             comment.save()
     return redirect("blog:post-detail", pk=pk)
+
+
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    fields = ("title", "content")
+    success_url = reverse_lazy("blog:index")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
