40ce14bb25c25c2788b08ea8c667b6baae8d63e4
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-04 20:08:37 +0200

    fix addition comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -5,14 +6,15 @@ from django.db import models
 
 class User(AbstractUser):
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Post(models.Model):
     owner = models.ForeignKey(
         to=get_user_model(),
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="posts"
     )
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -20,6 +22,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_time"]
+
+    def add_comment(self, user: User, commentary_content: str) -> Commentary:
+        comment = Commentary.objects.create(
+            user=user,
+            post=self,
+            content=commentary_content
+        )
+        return comment
 
     def __str__(self) -> str:
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,11 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.views import generic
 
-from blog.models import Post, Commentary
+from blog.models import Post
 
 
 class IndexView(generic.ListView):
     model = Post
-    queryset = Post.objects.all()
     paginate_by = 5
     template_name = "blog/index.html"
 
@@ -19,12 +18,11 @@ class PostDetailView(generic.DetailView):
 
 @login_required()
 def add_new_comment(request: HttpRequest, pk: int) -> HttpResponse:
-    user = request.user
     post = get_object_or_404(Post, pk=pk)
-    commentary_content = request.POST.get("new_comment")
-    Commentary.objects.create(
-        user=user,
-        post=post,
-        content=commentary_content
-    )
+
+    if request.method == "POST":
+        commentary_content = request.POST.get("new_comment")
+        if commentary_content:
+            post.add_comment(request.user, commentary_content)
+
     return redirect("blog:post-detail", pk=post.id)
