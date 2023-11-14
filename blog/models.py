6324661eaf3419322e8d23852d5bf9d29ba2466d
sasha <magazynuniversam@gmail.com>
2023-11-14 18:19:49 +0200

    add commentary display

--- blog/models.py
+++ blog/models.py
@@ -15,9 +15,17 @@ class Post(models.Model):
     content = models.TextField()
     created_time = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Commentary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_time = models.DateTimeField(default=timezone.now)
     content = models.TextField()
+
+
