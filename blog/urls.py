48af371cb84f47b7305df7a784be2c696d95e2fd
sasha <magazynuniversam@gmail.com>
2023-11-15 16:15:33 +0200

    add commentary widget

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, PostDetailView, create_comment
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail")
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/comment/', create_comment, name='create_comment'),
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.utils import timezone
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 
+from blog.forms import CommentaryForm
 from blog.models import Post
 
 
@@ -16,3 +20,21 @@ class PostDetailView(generic.DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
+
+@login_required
+def create_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentaryForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.created_time = timezone.now()
+            comment.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = CommentaryForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
