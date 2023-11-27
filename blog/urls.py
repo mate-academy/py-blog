7a6c55b5151a809ab1c11e549644f65606cbd051
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-11-27 21:03:34 +0200

    Fixed .gitignore and urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
-    path("posts//<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
