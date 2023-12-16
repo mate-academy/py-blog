3ed7c6458d998105a77da38e477ee569cf27dcc1
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-16 18:42:52 +0200

    Commented bootstrap4

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_bootstrap4",
+    # "crispy_bootstrap4",
     "crispy_forms",
     "blog",
 ]
