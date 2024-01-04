a46ca035cf49307b943bd0796d01f6d8c099ed4c
Vadym Tiron <tironvadim1@gmail.com>
2024-01-04 19:44:20 +0200

    'Solution'

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls", namespace="blog")),
 ]
