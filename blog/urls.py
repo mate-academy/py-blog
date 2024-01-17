f514b4355be47c99a502c756bf11692c8d08c4ac
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-01-17 20:26:51 +0200

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
-from blog.views import PostListView, PostDetailView, CommentCreateView
+from blog.views import PostDetailView, IndexListView, CommentaryCreateView
 from django.urls import path
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='index'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
-    path('posts/<int:pk>/comment/create/', CommentCreateView.as_view(), name="comment-create")
+    path("", IndexListView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/comment/create",
+         CommentaryCreateView.as_view(),
+         name="comment-create")
 ]
 
 app_name = "blog"
