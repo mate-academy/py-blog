291d2664df9db070ed1baee63a08af7f452e49bc
2ilga <2lgan1601@gmail.com>
2024-01-21 23:06:38 +0200

    настроил админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Post, Commentary
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+admin.site.register(Post)
+admin.site.register(Commentary)
--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 
+from blog_system import settings
+
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        ordering = ('username',)
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    content = models.CharField(max_length=20000)
+    content = models.TextField()
     created_time = models.CharField(max_length=255)
 
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models)
+class Commentary(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_time = models.CharField(max_length=255)
-    content = models.CharField(max_length=2000)
+    content = models.TextField()
