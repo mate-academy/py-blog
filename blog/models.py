b5e6011f81dcb298a606bdf7074d6881166a6a57
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-01-24 18:50:13 +0200

    "solution"

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,10 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
@@ -32,8 +35,16 @@ class Post(models.Model):
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="users"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
