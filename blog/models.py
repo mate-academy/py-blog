792cbf62034bb1fe329c5034712298bc2c32611b
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-01-24 18:56:44 +0200

    "solution"

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,15 @@ from blog_system import settings
 
 
 class User(AbstractUser):
-    # username = models.CharField(max_length=255, unique=True)
-    # email = models.EmailField(unique=True)
-    # first_name = models.CharField(max_length=255)
-    # last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ["username"]
+        verbose_name = "user"
+        verbose_name_plural = "users"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.username}"
