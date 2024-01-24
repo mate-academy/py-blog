2da4734a869364ccd895d06641660400178732aa
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-01-24 19:08:40 +0200

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,16 @@
 from .views import PostDetailView, IndexView, CommentaryCreateView
 from django.urls import path
 
+
 urlpatterns = [
-    path("", IndexView.as_view(), name="index",),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail",),
+    path("", IndexView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path(
-        "post/<int:pk>/comment/",
+        "posts/<int:pk>/comment/create/",
         CommentaryCreateView.as_view(),
-        name="comment-create",),
-    ]
+        name="comment-create"
+    ),
+]
 
 app_name = "blog"
