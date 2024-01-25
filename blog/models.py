87c3d93b8945d7ac0e03ade5cab0b089a7166a44
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-01-25 06:12:18 +0200

    models, initial migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(AbstractUser):
+    password = models.CharField(max_length=255)
+
+    class Meta:
+        pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    content = models.CharField(max_length=4096)
+    created_time = models.CharField(max_length=64)
+
+    class Meta:
+        pass
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_time = models.CharField(max_length=64)
+    content = models.CharField(max_length=4096)
+
+    class Meta:
+        pass
