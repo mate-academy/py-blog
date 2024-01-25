049ef58ed01f4eb09719c346aebb3a33b42e8097
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-01-25 06:48:21 +0200

    flake + tests. 8/22 steps

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
-
-
 class User(AbstractUser):
     password = models.CharField(max_length=255)
 
@@ -21,6 +18,12 @@ class Post(models.Model):
     class Meta:
         pass
 
+    def __str__(self):
+        return (
+            f"[{self.created_time}] "
+            f"Posted by {self.owner} [{self.title}]: {self.content}"
+        )
+
 
 class Commentary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -30,3 +33,9 @@ class Commentary(models.Model):
 
     class Meta:
         pass
+
+    def __str__(self):
+        return (
+            f"[{self.created_time}] Commentary by {self.user} "
+            f"under {self.post.owner}s post: {self.content}"
+        )
