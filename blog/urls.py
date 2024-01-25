3ae4bae845c9917ef5167f13a405660b0efb95e5
LeBwa <lebwa1337@gmail.com>
2024-01-25 20:05:58 +0200

    SolutionClass

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.static import static
 from django.urls import path
 
-from blog.views import PostListView, post_detail
+from blog.views import PostListView, PostDetailView
 from blog_system import settings
 
 urlpatterns = [
     path("post/", PostListView.as_view(), name="index"),
-    path("post/<int:pk>/", post_detail, name="post-detail"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
