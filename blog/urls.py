a776c476fa79b428ee4b5b377d87a3d297aa8cb6
Mikhail <tiunovmixs@gmail.com>
2024-02-07 13:56:08 +0200

    Blog implementation 2

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
-from blog.views import index
-
 from django.urls import path
+from .views import (
+    index,
+    PostDetailView,
+    PostListView,
+)
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
