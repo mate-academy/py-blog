3b06285af1cf1c31ebc84ce54bac25adb92258fb
Artur Poltser <polcerartur@gmail.com>
2024-02-24 16:56:49 +0200

    Solution

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views import generic
 
 from blog.forms import CommentaryForm
-from blog.models import Post, Commentary, User
+from blog.models import Post, Commentary
 
 
 class IndexListView(generic.ListView):
