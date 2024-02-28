8716853e8668828dea35de8e6259363b923ea2ed
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-02-28 11:00:10 +0200

    models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import User, Post, Commentary
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ("title", "owner",)
+    list_filter = ("created_time",)
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_filter = ("created_time",)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
