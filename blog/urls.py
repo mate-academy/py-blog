120f155d8e4d625e2c6d7e7e859c1b7a24f95cc7
Maxim Moshenskiy <osidiv@ukr.net>
2024-03-13 19:10:29 +0200

    added solution

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,11 @@ from blog.views import IndexListView, PostDetailView, CreateCommentView
 urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("comment/<int:pk>/", CreateCommentView.as_view(), name="comment-create"),
+    path(
+        "comment/<int:pk>/",
+        CreateCommentView.as_view(),
+        name="comment-create"
+    ),
 ]
 
 app_name = "blog"
