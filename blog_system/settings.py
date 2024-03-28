4de2c87501480c3aca35bb0390a3e9f1e0a062b1
Kavbian <124808381+Kavbian@users.noreply.github.com>
2024-03-28 21:41:14 +0200

    crispy

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
     "crispy_bootstrap4",
+    "crispy_forms",
     "blog",
 ]
 
