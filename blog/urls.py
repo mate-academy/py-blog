c27cf8755feda6f6c72bd086f037b16ba4baee2c
mkuchuman <mkuchuman1@gmail.com>
2024-03-29 23:20:00 +0100

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("create/", PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
-    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
 ]
 
 app_name = "blog"
