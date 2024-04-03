1555112b196c20ab02faa1e4a693a2d97f124c7f
Andrii Zubchenko <dissom1987@gmail.com>
2024-04-03 22:34:38 +0300

    Test

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,36 @@
-# from django.http import HttpRequest, HttpResponse
-# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import User, Post, Commentary
 
 
-class PostListView(generic.ListView):
+class PostListView(LoginRequiredMixin, generic.ListView):
     model = Post
     template_name = "blog/index.html"
-    context_object_name = "posts"
+    # context_object_name = "posts"
     queryset = Post.objects.order_by("-created_time")
     paginate_by = 5
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(LoginRequiredMixin, generic.DetailView):
     model = Post
     template_name = "blog/post_detail.html"
 
 
-class PostCreateView(generic.CreateView):
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
     model = Post
     fields = "__all__"
     template_name = "blog/post_form.html"
     success_url = reverse_lazy("blog:index")
 
 
-class PostDeleteView(generic.DeleteView):
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Post
     template_name = "blog/blog_confirm_delete.view"
     success_url = reverse_lazy("blog:index")
 
 
-class CommentaryCreateView(generic.CreateView):
+class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Commentary
     fields = "__all__"
     context_object_name = "comment-create"
@@ -39,5 +38,5 @@ class CommentaryCreateView(generic.CreateView):
     success_url = reverse_lazy("blog:index")
 
 
-class UserView(generic.ListView):
+class UserView(LoginRequiredMixin, generic.ListView):
     model = User
