98758c8267c503504892ed8244eccb1750f66c42
unknown <tarasyukmaxim@gmail.com>
2024-04-10 16:02:49 +0300

    Solution

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [п
+AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
