94fb7f9678bdb263db256583eccfc12d05f3ae6a
tiway <stilon670@gmail.com>
2024-05-03 19:38:40 +0300

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,11 @@ from blog.views import Index, PostDetailView
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
-    path("posts/<int:pk>/detail/", PostDetailView.as_view(), name="post-detail"),
+    path(""
+         "posts/<int:pk>/detail/",
+         PostDetailView.as_view(),
+         name="post-detail"
+         ),
 ]
 
 app_name = "blog"
