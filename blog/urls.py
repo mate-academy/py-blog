b59975d090a49bf2f34a815ec84d3945299bbcd7
xboris <boris.tikhonov.21@gmail.com>
2024-05-08 00:42:54 +0300

    'solution'

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from blog.views import PostListView, PostDetailView
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("", PostListView.as_view(), name="index"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+]
+
+app_name = "blog"
