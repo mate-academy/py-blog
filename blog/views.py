bd96c2091520cdba435171345f7e90842cdbe62d
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-05-10 16:39:37 +0200

    Solution

---
+++ blog/views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, ListView
+from django.core.paginator import Paginator
+
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.all().order_by("-created_time")
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.get_page(page_number)
+    context = {
+        "post_list": posts,
+        "is_paginated": True if posts.has_other_pages() else False,
+        "page_obj": posts,
+        "paginator": paginator,
+    }
+    return render(request, "blog/index.html", context=context)
+
+
+def comment_form(request, pk: int) -> HttpResponse:
+    if request.method == "POST":
+        form = CommentaryForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            post = Post.objects.get(pk=pk)
+            Commentary.objects.create(
+                user=user, post=post, content=form.cleaned_data["content"]
+            )
+            return redirect("blog:post-detail", pk=post.id)
+    form = CommentaryForm()
+    return render(request, "blog/post_detail.html", {"form": form})
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = "post_list"
+    template_name = "blog/post_list.html"
+    queryset = Post.objects.select_related("owner").order_by("-created_time")
+    paginate_by = 5
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    queryset = Post.objects.select_related("owner")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentaryForm()
+        return context
