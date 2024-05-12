e605f48bfb8925a390542edf225693394420b45c
NioviIki <charmniovi@tutanota.com>
2024-05-12 17:05:12 +0300

    added models in blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ("email", "first_name", "last_name", "password")
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=66)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=150)
