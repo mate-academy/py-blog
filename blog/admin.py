964139db591f58df0381f5d79254de1ae8c75f7a
NioviIki <charmniovi@tutanota.com>
2024-05-12 17:35:24 +0300

    added admins view in blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import User, Post, Commentary
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class DriverAdmin(UserAdmin):
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("username", "email", "first_name", "last_name")
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("owner", "title", "created_time")
+    search_fields = ("owner", "title", "content", "created_time")
+    list_filter = ("owner", "title", "content", "created_time")
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "created_time")
+    search_fields = ("user__username", "post__title", "post__owner__username", "created_time")
+    list_filter = ("user", "post__title", "created_time")
--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     REQUIRED_FIELDS = ("email", "first_name", "last_name", "password")
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=66)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.title} by {self.owner} at {self.created_time.strftime('%Y-%m-%d')}"
+
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentary")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commentary")
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=150)
