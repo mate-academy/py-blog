95e0d66e6c154100ae774ec92c2295d236e8985b
NioviIki <charmniovi@tutanota.com>
2024-05-12 21:54:12 +0300

    added IndexView and PostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import IndexView, PostDetailView
+
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("post/<int:pk>", PostDetailView.as_view(), name="post-detail")
+]
 
 app_name = 'blog'
