0ccbd5a738cee3c0dde5ec88ed3bf1f6fce9ea56
skeletronys <mykytcey@gmail.com>
2024-05-23 18:28:15 +0200

    fixed, flake8 and add button in home page

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
