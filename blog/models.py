2c4051553ef71e5f8eb9e0d63cfe56b2c3437252
skeletronys <mykytcey@gmail.com>
2024-05-24 15:25:03 +0200

    fix

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class User(AbstractUser):
 
 class Post(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE,
+                              related_name='posts')
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
