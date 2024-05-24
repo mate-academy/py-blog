751733a5dbf5b7dbfc587c6afe697af755ff3c8e
skeletronys <mykytcey@gmail.com>
2024-05-24 16:48:06 +0200

    fix

--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,20 @@ from blog_system import settings
 
 
 class User(AbstractUser):
-    email = models.CharField(max_length=63)
+    email = models.EmailField(max_length=255)
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE,
-                              related_name="posts")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
@@ -31,9 +36,14 @@ class Post(models.Model):
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
