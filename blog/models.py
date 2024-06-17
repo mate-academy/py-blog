50471be84d99d5bed227073a24453f161a5af3eb
Alona <alona.sorochynska@gmail.com>
2024-06-17 07:42:58 +0100

    change User to AUTH_USER_MODEL in settings, related names, migrate

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from blog_system import settings
+
 
 class User(AbstractUser):
     pass
@@ -10,24 +12,30 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User,
-                              on_delete=models.CASCADE,
-                              related_name="posts")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
 
     def __str__(self):
         return self.title
 
     def comment_count(self):
-        return self.commentary.count()
+        return self.commentaries.count()
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name="commentary")
-    post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name="commentary")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
