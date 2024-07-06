30f123ac59a07e950449c59535b8d71c1091d9d1
Anton Bilous <anton.bilosus@gmail.com>
2024-07-06 15:30:08 +0300

    Add blog/urls.py

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from .views import PostListView, PostDetailView, commentary_create_view
+
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path(
+        "posts/<int:pk>/create-comment",
+        commentary_create_view,
+        name="post-create-comment",
+    ),
+]
