d3a67619c93444e9b29f4693c8712d5be10c6983
Anton Bilous <anton.bilosus@gmail.com>
2024-07-06 13:51:22 +0300

    Add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=2047)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created_time",)
+
+    def __str__(self):
+        return f"{self.title} (by {self.owner.username})"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=2047)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created_time",)
+        verbose_name_plural = "commentaries"
+
+    def __str__(self):
+        return f"{self.content} (by {self.user.username})"
