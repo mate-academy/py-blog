781e3236eddcb939a5a68568a48e465ba47bcae2
Anton Bilous <anton.bilosus@gmail.com>
2024-07-06 15:31:59 +0300

    Configure crispy integration

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "blog",
 ]
 
@@ -127,3 +129,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "blog.User"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
