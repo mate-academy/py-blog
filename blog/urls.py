0e47278192cc54b0ff8cfbb005ef1b85ff58eade
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-07 15:12:48 +0300

    create detail page and add commentary

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, PostDetailView,create_commentary_view
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path(
+        "posts/<int:pk>/create/",
+        create_commentary_view,
+        name="commentary-create"
+    ),
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 
-from blog.models import Post
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
 
 
 class PostListView(generic.ListView):
@@ -12,3 +15,20 @@ class PostListView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = Post
+    queryset = Post.objects.select_related("owner").prefetch_related("comments__user")
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentaryForm()
+        return context
+
+
+@login_required
+def create_commentary_view(request: HttpRequest, pk: int) -> HttpResponseRedirect:
+    if request.method == "POST":
+        form = CommentaryForm(request.POST)
+        if form.is_valid():
+            Commentary.objects.create(
+                post_id=pk, user=request.user, **form.cleaned_data
+            )
+    return HttpResponseRedirect(reverse("blog:post-detail", args=(pk,)))
