ab3d1e701a5c9b66933f1a40cd60abbae650da2a
haldaniko <haldaniko@gmail.com>
2024-08-16 16:29:10 +0200

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from blog.models import Post
 
 
 class PostListView(generic.ListView):
+    paginate_by = 5
     model = Post
     queryset = (Post.objects.select_related("owner")
                 .prefetch_related("commentary_set"))
