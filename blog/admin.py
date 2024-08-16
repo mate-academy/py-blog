d15c0e91831ed5cc5f5804534067ee76ba8d3c74
haldaniko <haldaniko@gmail.com>
2024-08-16 15:32:28 +0200

    register 'Post' and 'Commentary' in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Post, Commentary
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created_time')
+    search_fields = ('title', 'content')
+    list_filter = ('created_time', 'owner')
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_time')
+    search_fields = ('content',)
+    list_filter = ('created_time', 'user')
