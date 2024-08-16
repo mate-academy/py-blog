924bbd75f367fb0956dcbf0abd23ad6dfda80563
haldaniko <haldaniko@gmail.com>
2024-08-16 15:25:00 +0200

    create models 'Commentary' and 'Post'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.title} by {self.owner}"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=1024)
