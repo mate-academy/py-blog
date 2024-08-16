a95d5d9ad54316973f676c9359c02d1aaa9308d3
haldaniko <haldaniko@gmail.com>
2024-08-16 15:39:09 +0200

    add 'User' model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,23 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Change 'custom_user_set' to any unique name
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions '
+                   'granted to each of their groups.'),
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Change 'custom_user_permissions_set' to any unique name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='user',
+    )
 
 
 class Post(models.Model):
