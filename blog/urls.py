c1cf464683df11939186a652d79afa008f8e4872
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-08-29 22:18:15 +0300

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
-from .views import PostListView, PostDetailView
+from django.urls import path
+
+from blog.views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="index"),
-    path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts", PostListView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
