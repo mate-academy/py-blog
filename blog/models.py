f72af7508361454ae39bf5282dccdd636797950a
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-11 13:31:20 +0300

    Fix

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
-from django.conf import settings
+
 
 class User(AbstractUser):
     class Meta:
