f81d6527d372e93dd2de8d31178e5487cfba16ae
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-09-13 15:55:23 +0300

    admin panel implemented

---
+++ blog/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Post, Commentary
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                    )
+                },
+            ),
+        )
+    )
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "owner", "created_time"]
+    search_fields = ("title", "content")
+    list_filter = ("owner", "created_time")
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ["content","post", "created_time"]
+    list_filter = ("user",)
+    search_fields = ("content",)
