ac6eeb7467d238a58af52f9dd497382fe6024f05
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-14 20:37:50 +0300

    delete toolbar

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -38,14 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    "debug_toolbar",
     "crispy_forms",
     "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- blog_system/urls.py
+++ blog_system/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls", namespace="blog")),
-] + debug_toolbar_urls()
+]
