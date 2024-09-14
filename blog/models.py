38748156b2efa776a2a117f0d365d0a12124631a
Andrii <andrii.khamaza0@gmail.com>
2024-09-14 22:35:21 +0300

    Fixed flake8 mistakes

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,10 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
@@ -18,7 +21,10 @@ class Post(models.Model):
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="commentaries"
     )
