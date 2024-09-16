74589474b1087dcd2ce2e0c1be2d8e3716099406
Alexandr Politov <apolitov83@gmail.com>
2024-09-16 16:04:55 +0300

    Reworked everything

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
