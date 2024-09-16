0f34016d148a2bb706d7d7a802694fe8b0a35dc2
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-16 15:27:03 +0300

    Hmm

--- blog/admin.py
+++ blog/admin.py
@@ -2,21 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from blog.models import User, Post, Commentary
-
-admin.site.unregister(Group)
-admin.site.register(Commentary)
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ["owner", "title", "created_time"]
-    list_filter = ["title", "owner", "created_time"]
-    search_fields = ["owner", "title"]
+from blog.models import Commentary, Post, User
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = UserAdmin.list_display
-    list_filter = UserAdmin.list_filter
-    add_fieldsets = UserAdmin.add_fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "email")}),
+    )
+
+
+admin.site.register(Post)
+admin.site.register(Commentary)
+admin.site.unregister(Group)
