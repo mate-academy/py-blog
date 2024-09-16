256e18d5d29446b1ab33e0108cb73534849416cd
Alex Shcherbakov <oleksa.shcherbakov@gmail.com>
2024-09-16 20:36:59 +0300

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from blog.models import Commentary
 from blog.views import (
     PostListView,
     PostDetailView,
@@ -12,11 +11,23 @@ from blog.views import (
 
 urlpatterns = [
     path("blog/create/", PostCreateView.as_view(), name="post-create"),
-    path("blog/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
-    path("blog/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path(
+        "blog/<int:pk>/update/",
+        PostUpdateView.as_view(),
+        name="post-update"
+    ),
+    path(
+        "blog/<int:pk>/delete/",
+        PostDeleteView.as_view(),
+        name="post-delete"
+    ),
     path("", PostListView.as_view(), name="index"),
     path("blog/<int:pk>", PostDetailView.as_view(), name="post-detail"),
-    path("blog/<int:pk>/create_comment", CommentaryCreateView.as_view(), name="comment-create"),
+    path(
+        "blog/<int:pk>/create_comment",
+        CommentaryCreateView.as_view(),
+        name="comment-create"
+    ),
 
 ]
 
