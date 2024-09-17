331841450a7ce51648a4b8237553e7a0b92e3398
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2024-09-17 08:35:59 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from blog.views import Index, PostDetailView, add_commentary
 
+
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
