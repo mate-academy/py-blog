15143c95c923654023bf55558fd63679fe05ebb0
Halyna Baklanova <novabalka@gmail.com>
2024-09-20 16:22:15 +0300

    Solution

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic import CreateView
 
 from blog.models import Post, Commentary, User
 from blog.form import CommentaryForm
