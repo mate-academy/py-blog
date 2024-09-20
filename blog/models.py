eae7d7d3bf0fcacd8bc6143d8292335e9389439f
Halyna Baklanova <novabalka@gmail.com>
2024-09-20 16:37:09 +0300

    Solution

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,28 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="post"
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentary")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commentary")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="commentary"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="commentary"
+    )
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
