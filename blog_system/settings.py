adfa7545b5bd81ee4ea15c0f2370f3556c8b87bc
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-09-27 12:43:32 +0300

    fix settings.py

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    "crispy_bootstrap4",
     "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
