c3957440fd63a26b884d153f7c2abd1ef9d2da6f
Vlad Rymarchuk <vladrymarchuk@gmail.com>
2024-10-06 19:45:20 +0300

    Created a template for the main page. On this page, make it so that a list of all posts is displayed. The title and content were displayed, the author was visible when this post was created and the number of comments on the post, added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from blog.models import Post, Commentary, User
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.all().order_by("-created_time")
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+        "is_paginated": page_obj.has_other_pages(),
+        "commentaries": Commentary.objects.select_related(),
+        "users": User.objects.all()
+    }
+
+    return render(
+        request,
+        "blog/index.html",
+        context=context
+    )
