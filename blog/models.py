6d797457257a86f015f1e51e701bdcede8fe5dfd
Vlad Rymarchuk <vladrymarchuk@gmail.com>
2024-10-06 15:14:29 +0300

    Berore migrations: Created models and registered models in admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from blog_system import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self) -> str:
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=255)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.owner}: ({self.title})"
+
+
+class Commentary(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="commentaries"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
