ebd42598a1506bc957c5e444c13b4ad77d248419
Semyon Yerep <yerepsemyon@gmail.com>
2024-10-08 22:58:57 +0300

    changed to class based view

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,10 @@ from .views import (
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("post/<int:post_id>/add-comment/", CommentCreateView.as_view(), name="add-comment"),
+    path(
+        "post/<int:post_id>/add-comment/",
+        CommentCreateView.as_view(), name="add-comment"
+    ),
 ]
 
 app_name = "blog"
