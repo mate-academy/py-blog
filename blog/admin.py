332dc2f21ffe8371ede53d9bafad425eb08716a7
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-10-09 08:30:58 +0300

    Edit admin.py: Register all your models in the admin. Unregister Group.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from blog.models import Post, Commentary, User
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.fields]  # Lists all fields in User model
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.fields]  # Lists all fields in Post model
+    list_filter = ['owner', 'created_time']
+    search_fields = ['title', 'content']
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Commentary._meta.fields]  # Lists all fields in Commentary model
+    list_filter = ['user', 'created_time', 'post__title']
+    search_fields = ['content']
