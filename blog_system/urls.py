98f3829182e50810107ec4d592c32d6aeea0f9ee
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-10-09 08:57:10 +0300

    added urls for blog app

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
