d1f227ec24fc0098a8125960b1e2d9b6b8ff65d4
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-10-09 16:44:29 +0300

    Add pagination for the main page. Set 5 posts on one page by default.
    Create a PostDetailView view that returns detailed information about the post by the id field.
    
    Add a template for this page and url with a path posts/pk/ and name post-detail.
    
    On the post detail page, display a list of post comments below this post.
    
    Under the list of comments, add a form that allows you to create a new comment to the post.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from blog.views import PostListView, PostDetailView
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='index'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/', PostListView.as_view(), name='index'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 
+from blog.forms import CommentaryForm
 from blog.models import Post
 
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentaryForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentaryForm(request.POST)
+        post = self.get_object()
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user  # Assign the current user to the comment
+            comment.post = post  # Assign the current post to the comment
+            comment.save()
+            return redirect('blog:post-detail', pk=post.pk)  # Redirect back to the post detail page
+
+        # If the form is not valid, re-render the page with the form errors
+        return self.get(request, *args, form=form)
