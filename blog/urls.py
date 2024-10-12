5de4dc3bd19c5271b7a04c663e4246332719d753
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-10-12 17:42:57 +0300

    Update

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from blog.views import PostListView, post_detail_view
+urlpatterns = [
+    path("posts/", PostListView.as_view(), name="index"),
+    path("posts/<int:pk>/", post_detail_view, name="post-detail")
+]
+
+app_name = "blog"
---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
+
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
+
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.prefetch_related("commentaries")
+    paginate_by = 5
+
+
+def post_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    user = request.user
+    post = Post.objects.prefetch_related("commentaries__user").get(id=pk)
+    form = CommentaryForm(request.POST or None)
+    if form.is_valid():
+        form.cleaned_data["user"], form.cleaned_data["post"] = user, post
+        Commentary.objects.create(**form.cleaned_data)
+
+        return HttpResponseRedirect(
+            reverse("blog:post-detail", kwargs={"pk": pk})
+        )
+
+    return render(
+        request, "blog/post_detail.html", context={"post": post, "form": form}
+    )
