748d5910a7032100f7b9cebac0835120816e55b9
Arsen <arsen.test.mess@gmail.com>
2024-10-16 08:48:39 +0300

    fix flake8 errors

--- blog/admin.py
+++ blog/admin.py
@@ -10,10 +10,12 @@ class UserAdmin(UserAdmin):
     search_fields = ("username", "email",)
     list_filter = ("username", "email",)
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ("title",)
     list_filter = ("title",)
+
 
 @admin.register(Commentary)
 class CommentaryAdmin(admin.ModelAdmin):
