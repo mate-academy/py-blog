c3eca488dd0c0f62ecfb3ffd3c6ccf6e3e673f75
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-10-20 18:44:33 +0300

    "sol"

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
     path("accounts/", include("django.contrib.auth.urls")),
-
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
