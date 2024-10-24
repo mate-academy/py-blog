c1334a054d4e85aae8201bf721111d96dd710e0e
Roman Sokolov <gnonasis@gmail.com>
2024-10-24 16:58:33 +0300

    Solution ver4

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("VARABLE", "dummy-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("VARABLE", "dummy-key")
+DEBUG = "True"
 
 ALLOWED_HOSTS = []
 
