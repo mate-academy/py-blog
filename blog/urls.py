1a9d658ab198632492f924cdfb833fa67c326516
Viacheslav417865 <sfinks417865@gmail.com>
2024-10-28 11:24:48 +0200

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import index, PostDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("blog/", RedirectView.as_view(url="/blog/", permanent=False), name="redirect"),
 ]
