1a7851695c69cb59ba1921b4a445567d2ed40554
Mykola <jacktyler2391@gmail.com>
2024-10-29 17:03:07 +0200

    fix flake8

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 
 from blog.views import PostListView, PostDetailView, CommentaryCreateView
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="index"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/comment/", CommentaryCreateView.as_view(), name="create-comment"),
+    path("", PostListView.as_view(),
+         name="index"),
+
+    path("posts/<int:pk>/",
+         PostDetailView.as_view(),
+         name="post-detail"),
+
+    path(
+        "posts/<int:pk>/comment/",
+        CommentaryCreateView.as_view(),
+        name="create-comment"
+    ),
 ]
