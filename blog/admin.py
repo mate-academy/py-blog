666a77b013218fb1552fc3bbd0b0ec93bcc3067c
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-11-09 15:22:47 +0200

    start project, add app and crqate models. Registr models in admin.py

---
+++ blog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from blog.models import Post, Commentary, User
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["owner", "title", "created_time"]
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
