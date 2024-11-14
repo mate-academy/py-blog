29746e0f0d56a813c41684715d56e90b6ccdcd7a
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-11-14 16:59:41 +0200

    Solution

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 from django.conf import settings
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-%mp)6e6)*7j$zyj8k&p6m$5ex^-2-9%zpf1dlioyf_duw7xmb("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
