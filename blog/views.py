840b7fcfcd15eec41c1ab52d0781455ad599a19b
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-23 14:03:35 +0200

    Blog v1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
+from django.db.models import Count
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import User, Post, Commentary
+
+def index(request: HttpRequest) -> HttpResponse:
+    user_count = User.objects.count()
+    post_count = Post.objects.count()
+    comment_count = Commentary.objects.count()
+    posts = Post.objects.annotate(comments_count=Count("comments")).order_by("-created_time")
+
+    context = {
+        "user_count": user_count,
+        "post_count": post_count,
+        "comment_count": comment_count,
+        "posts": posts,
+    }
+
+    return render(request, "blog/index.html", context=context)
