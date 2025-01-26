8632bfc9cb9743e8a06cef1fc0f2737c0969e1f4
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-01-26 21:07:19 +0200

    first version

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, PostDetailView, create_comment_view
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/comment", create_comment_view, name="create-comment"),
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.db.models.aggregates import Count
+from django.urls import reverse
 from django.views import generic
 
-from blog.models import Post
+from blog.models import Post, Commentary
+from blog.forms import CommentaryForm
 
 
 
@@ -17,3 +19,25 @@ class PostListView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = Post
+    queryset =  Post.objects.select_related("owner")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["com_form"] = CommentaryForm()
+        return context
+
+
+def create_comment_view(request, pk: int):
+    post_obj = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        form = CommentaryForm(request.POST)
+        if form.is_valid():
+            commentary = Commentary()
+            commentary.post = post_obj
+            commentary.user = request.user
+            commentary.content = form.cleaned_data["comment"]
+            commentary.save()
+            return redirect("blog:post-detail", pk=pk)
+    else:
+        form = CommentaryForm()
+        return render(request, "blog/post_detail.html", {"post": post_obj, "com_form": form })
