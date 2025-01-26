322bd3c366b0c64f9188b326e257bf7d9c6fcef4
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-01-26 14:56:38 +0100

    Add models: [User], [Post], [Commentary] and create AUTH_USER_MODEL

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Meta:
+        ordering = ("username", )
+
+class Post(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("created_time", )
+
+class Commentary(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("created_time", )
