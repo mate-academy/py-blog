aaf8c1a3fa58d5570fc560dd9f9d6cf3ce85fff5
4ikkamony <compbanderloga@gmail.com>
2025-01-27 22:15:54 +0200

    configured debug_toolbar, crispy. Used black to format generated files

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+] + debug_toolbar_urls()
