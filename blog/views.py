2f348e10650640a669f1bd54ba29a7f83b769599
4ikkamony <compbanderloga@gmail.com>
2025-01-29 17:08:34 +0200

    add urlpatterns for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Hello world</h1>")
--- blog_system/urls.py
+++ blog_system/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("", include("blog.urls", namespace="blog")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls"))
 ]
