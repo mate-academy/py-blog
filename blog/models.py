14411614ba471e3fb145d82bc5bf387221ef5cba
Julia Alexeeva <julia4406@gmail.com>
2025-01-30 19:11:42 +0200

    Initial commit. Models created and registered in admin.py

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import CASCADE
+
+
+class User(AbstractUser):
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (f"{self.title} by user "
+                f"({self.owner.first_name} {self.owner.last_name})")
+
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=CASCADE, related_name="comments")
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "commentaries"
+
+    def __str__(self):
+        return {self.content}
+
