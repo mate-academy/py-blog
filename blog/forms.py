93aa2d69b588bfdde9eb0f78cb16f0212e9bde63
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-01-31 03:18:21 +0200

    first_solution_need_to_fix_test

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from blog.models import Commentary
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Commentary
+        fields = ["content"]
+        widgets = {
+            "content": forms.Textarea(
+                attrs={"rows": 3, "placeholder": "Write your comment"}
+            ),
+        }
