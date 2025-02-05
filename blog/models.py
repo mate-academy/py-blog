55c1774e85af6d65760c2d5c9f7f29a033ab3649
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-02-05 20:55:22 +0100

    Fix ordering of models and implement __str__ methods

--- blog/models.py
+++ blog/models.py
@@ -2,24 +2,42 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     class Meta:
         ordering = ("username", )
 
+    def __str__(self):
+        return f"First Name: {self.first_name}, Last Name: {self.last_name}"
+
+
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ("created_time", )
+        ordering = ("-created_time", )
+
+    def __str__(self):
+        return self.title
+
 
 class Commentary(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ("created_time", )
+        ordering = ("-created_time", )
+
+    def __str__(self):
+        return f"User: {self.user}: {self.content}"
