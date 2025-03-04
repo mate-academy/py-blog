91524f5f8800762deb652339662737ab70c15dd8
Artem <stilon670@gmail.com>
2025-03-04 09:20:27 +0200

    Remove comments

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,9 @@
-from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 
 from blog.forms import CommentaryForm
-from blog.models import Post, Commentary
-
-
-# def index(request: HttpRequest) -> HttpResponse:
-#     posts = Post.objects.order_by("-created_time")
-#     paginator = Paginator(posts, 4)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-#     context = {"posts": posts, "page_obj": page_obj}
-#     return render(request, "blog/post_list.html", context)
+from blog.models import Post
 
 
 class Index(generic.ListView):
