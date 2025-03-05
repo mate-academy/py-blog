b304dfb009acfa4c72b7ea397d6f1bf6a53b5f8f
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-05 21:41:12 +0100

    Create index view for homepage in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.all().order_by("-created_time")
+    context = {
+        "post_list": post_list
+    }
+    return render(request, "blog/index.html", context=context)
