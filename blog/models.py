4e14ebec22e0df98858926d9e3261bbd46289ada
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-09 14:48:07 +0100

    Add related_name to ForeignKey fields

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class User(AbstractUser):
 class Post(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="posts",
     )
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -30,9 +31,14 @@ class Post(models.Model):
 class Commentary(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="comments",
     )
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
