eba679c9c04754b0794841caf677d9ff062e8d77
RuslanTopal <baranotik15@gmail.com>
2025-03-11 17:21:53 +0000

    add base view and url path + create base templates and static directory with future structure

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
