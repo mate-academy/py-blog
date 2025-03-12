c50afb6eefcca96818e1eeeaf7fb5bb8690a3aa8
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-12 15:47:28 +0100

    Set models and add register them in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import User, Post, Commentary
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_time",)
+    search_fields = ("title", "content",)
+    list_filter = ("title", "created_time",)
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "user", "created_time",)
+    search_fields = ("content",)
+    list_filter = ("created_time",)
+
+
+@admin.register(User)
+class AdminUser(UserAdmin):
+    pass
+
+admin.site.unregister(Group)
