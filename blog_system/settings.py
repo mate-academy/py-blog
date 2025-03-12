3f4164bdd04cfbd73636890c99f4f2da7ea809fe
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-12 15:47:28 +0100

    Set models and add register them in admin.py

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "blog.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
