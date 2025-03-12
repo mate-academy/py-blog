69b7f408b2d27448ce81b86650b34b48482c7766
RuslanTopal <baranotik15@gmail.com>
2025-03-12 06:42:22 +0000

    Add bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
 
-from .models import Post
+from .models import Post, Commentary
 
 
 def index(request):
@@ -18,7 +20,32 @@ def index(request):
     return render(request, "blog/index.html", context=context)
 
 
-class PostDetailView(DetailView):
+class CommentaryForm(forms.ModelForm):
+    class Meta:
+        model = Commentary
+        fields = ['content']
+
+
+class PostDetailView(FormMixin, DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
+    form_class = CommentaryForm
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comments = Commentary.objects.filter(post=post)
+        context['comments'] = comments
+
+        return context
+
+    def form_valid(self, form):
+        post = self.get_object()
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return redirect('post_detail', pk=post.pk)
+
+
--- blog_system/settings.py
+++ blog_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'blog',
 ]
 
@@ -128,3 +130,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'blog.User'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
