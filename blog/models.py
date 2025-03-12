80dd79d5453efa95c4c16cf2bb6eef2494e1946d
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2025-03-12 12:08:29 +0200

    created models user as AbstractUser Post and Commentary

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
