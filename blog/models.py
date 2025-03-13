c5b4d3893c37efa1c555294cd804b47960a5a0ff
Tetiana Sobko <sobkotiana@gmail.com>
2025-03-13 22:21:05 +0200

    Fix flake8 errors

--- blog/models.py
+++ blog/models.py
@@ -15,10 +15,10 @@ class Post(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created_time']
+        ordering = ["created_time"]
 
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.pk})
+        return reverse("post_detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.title
@@ -26,9 +26,16 @@ class Post(models.Model):
 
 class Commentary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
     def __str__(self):
-        return f"{self.user} on {self.post}: {self.content[:100] + '...' if len(self.content) > 100 else self.content}"
+        return (
+            f"{self.user} on {self.post}: "
+            f"{self.content[:100] + "..." if (
+                len(self.content) > 100
+            ) else self.content}"
+        )
