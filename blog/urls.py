70a2280618be4c694815503d20952222a70025e0
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-13 18:53:35 +0100

    Add the index view and base templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import (
-    PostListView, PostDetailView, SignUpView, CommentaryCreateView, PostCreateView, PostUpdateView, PostDeleteView,
-    MyPostsListView, about_view
-)
+from .views import PostListView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
-    path("my-posts/", MyPostsListView.as_view(), name="my-posts"),
-    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
-    path("posts/<int:pk>/post-comment", CommentaryCreateView.as_view(), name="commentary-create"),
-    path("posts/create", PostCreateView.as_view(), name="post-create"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/sign-up/", SignUpView.as_view(), name="sign-up"),
-    path("about/", about_view, name="about")
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "blog/index.html"
