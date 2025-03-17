c0b329e9cf61bc42ea0beba5de0327bd60fed700
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-17 15:13:39 +0100

    Implement post detail view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,10 @@ class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.select_related("owner")
     template_name = "blog/index.html"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.select_related("owner").prefetch_related("commentary_set__user")
