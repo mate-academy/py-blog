cded50467f10ad169d975e9fada85f39e4757731
shafran <shafran.andriy@gmail.com>
2025-03-18 23:31:10 +0200

    Created all files and classes

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog import views
+from blog.views import (index,
+                        PostDetailView)
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts/pk/", PostDetailView.as_view(), name="post-detail"),
+]
