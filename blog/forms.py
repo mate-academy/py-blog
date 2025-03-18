506b637817627a46fa0962f7d1bc2e57cbcef9fd
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-18 19:16:00 +0100

    Implement login, logout and sign up functionality

--- blog/forms.py
+++ blog/forms.py
@@ -1,26 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import Textarea
-from django.forms.models import ModelForm
-
-from blog.models import Commentary, Post
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ("username", "email")
-
-
-class CommentaryForm(ModelForm):
-    class Meta:
-        model = Commentary
-        fields = ("content",)
-        labels = {"content": "Post a comment",}
-        widgets = {"content": Textarea(attrs={"cols": 20, "rows": 3}),}
-
-
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ("title", "content",)
+        fields = ('username', 'email')  # Add or remove fields as necessary
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
+from blog.forms import CustomUserCreationForm
 from blog.models import Post
 
 
 class PostListView(generic.ListView):
     model = Post
-    queryset = Post.objects.select_related("owner")
+    queryset = Post.objects.select_related("owner").prefetch_related("commentary_set")
     template_name = "blog/index.html"
 
 
@@ -15,3 +18,18 @@ class PostDetailView(generic.DetailView):
 
     def get_queryset(self):
         return Post.objects.select_related("owner").prefetch_related("commentary_set__user")
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    model = get_user_model()
+    template_name = "registration/sign_up.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse("blog:index")
