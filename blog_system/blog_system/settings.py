4caf0df1c6fe7d0712b07cd23bfd9572c4b4345d
shafran <shafran.andriy@gmail.com>
2025-03-19 14:06:49 +0200

    Created all files and classes

--- blog_system/blog_system/settings.py
+++ blog_system/blog_system/settings.py
@@ -88,7 +88,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME":
-            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+            "django.contrib."
+            "auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
         "NAME":
