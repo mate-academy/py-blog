9e05d58443dcb45c8f48f93aac9a460f3eac59ba
shafran <shafran.andriy@gmail.com>
2025-03-20 19:07:34 +0200

    Created project and app

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from blog import views
+from blog.views import index, PostDetailView
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+]
+
+
+app_name = "blog"
