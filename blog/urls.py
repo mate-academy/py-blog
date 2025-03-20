c04430ac34d673f5250bfe801eb843e25df0dbf8
misha-doroshenko <doroshenko.misha73@gmail.com>
2025-03-20 15:05:38 +0100

    Implement create, update, delete functionality for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 
-from .views import PostListView, PostDetailView, SignUpView, CommentaryCreateView, PostCreateView
+from .views import (
+    PostListView, PostDetailView, SignUpView, CommentaryCreateView, PostCreateView, PostUpdateView, PostDeleteView
+)
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
     path("posts/<int:pk>/post-comment", CommentaryCreateView.as_view(), name="commentary-create"),
     path("posts/create", PostCreateView.as_view(), name="post-create"),
     path("accounts/", include("django.contrib.auth.urls")),
