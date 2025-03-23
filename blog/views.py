dc34fdba0b11793427ef4347025c9c807ce762e0
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-23 19:51:44 +0100

    Add pagination to PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
@@ -6,7 +7,13 @@ from blog.models import Post
 
 def index(request: HttpRequest) -> HttpResponse:
     post_list = Post.objects.all().order_by("-created_time")
+
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get("page")
+    page_obj = paginator.get_page(page)
     context = {
-        "post_list": post_list
+        "post_list": page_obj,
+        "is_paginated": page_obj.has_other_pages(),
+        "paginator": paginator,
     }
     return render(request, "blog/index.html", context=context)
