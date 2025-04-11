277d384ccfb429061e24b79258756f47ad22db6b
Dmytro Stehantsev <stegan296@gmail.com>
2025-04-11 12:09:50 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from blog.views import index, PostDetailView
 
 urlpatterns = [
-    path("index/", index, name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("index/", index, name="index"),
 ]
 
 app_name = "blog"
