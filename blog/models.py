049e20dc2a606097aa6934431e84a6720f49dc6e
Бронислав <atlantwindow@gmail.com>
2025-04-12 15:54:22 +0300

    Create BlogSystem

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,10 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(default=timezone.now)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts")
 
     class Meta:
         ordering = ["-created_time"]
