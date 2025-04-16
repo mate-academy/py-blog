3edd03887081654c4fa765114e16f535703ed992
Liza Shulika <liza.shulika2009@gmail.com>
2025-04-16 13:21:22 +0300

    blog

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from .views import (
+  index,
+  PostDetailView,
+)
+from django.urls import path
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+  ]
+
+app_name = "blog"
