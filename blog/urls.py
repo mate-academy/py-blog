a7212746c76035acdba7568290a4a47bfca253a2
Liza Shulika <liza.shulika2009@gmail.com>
2025-04-16 22:39:38 +0300

    blog2

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from .views import (
-  index,
-  PostDetailView,
+    index,
+    PostDetailView,
+    PostCreateView,
+
 )
 from django.urls import path
 
@@ -8,6 +10,8 @@ from django.urls import path
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-  ]
+    path("posts/create/", PostCreateView.as_view(),
+         name="post-create"),
+    ]
 
 app_name = "blog"
