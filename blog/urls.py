fca25152f52c4da6f52f70cf17b64fc1592efa7a
Aleksander Rokita <kelor2@o2.pl>
2025-05-04 16:33:29 +0200

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
     # path("", IndexView.as_view(), name="index"),
-    path("index/", index, name="index"),
-    path("posts/", index, name="index"),
+    path("", index, name="index"),
     # path("posts/", IndexView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
