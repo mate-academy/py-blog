7d0ce1d5bd1d52a981fefd88ea90d7079931f2c7
Max <smolinskijmaksim1@gmail.com>
2025-05-09 17:09:50 +0100

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,4 @@ app_name = "blog"
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("comment/create/", PostDetailView.as_view(), name="comment-create"),
-
 ]
