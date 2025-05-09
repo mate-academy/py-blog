3f9c2eaab5673748a5f6af706bcb08adc0d2ad47
Miwacer <il>
2025-05-09 23:44:50 +0300

    Sulution

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
     path(
         "posts/<int:pk>/create_comment/",
-         create_comment,
+        create_comment,
         name="create-comment"
-    ),
+    )
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import render, redirect
 from django.views import generic
 
 from .models import Post, Commentary, User
@@ -34,6 +34,10 @@ def create_comment(request: HttpRequest, pk: int) -> HttpResponse:
                 user=request.user,
                 content=content
             )
+        elif request.method == "GET":
+            return redirect("blog:post-detail", pk=pk)
+        else:
+            return HttpResponseNotAllowed(["GET", "POST"])
 
     return redirect("blog:post-detail", pk=pk)
 
