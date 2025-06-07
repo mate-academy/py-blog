8c3c56d26112b8ea9a2ad9026935ebff69a674b0
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-06-07 17:09:40 +0200

    feat: unregister Group in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from .models import User, Post, Commentary
 
+admin.site.unregister(Group)
 
 # Register your models here.
 @admin.register(User)
