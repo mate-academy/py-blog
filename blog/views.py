e75315254fc2c60bff519c76d4c61d1c4f5c26db
Denys Bokov <denisbokov1703@gmail.com>
2025-06-07 17:33:10 +0200

    feat: add initial post list view with configured templates and static settings

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/post_list.html'
