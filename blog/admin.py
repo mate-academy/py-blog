30341643e59b62346a0773a0999469ceacf1e50e
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-06-07 16:23:46 +0200

    feat: create user model and register in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdministrator(UserAdmin):
+    list_filter = UserAdmin.list_filter + ("username", 'first_name', 'last_name')
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Information', {'fields': ('first_name', 'last_name', 'email')})
+    ),
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    def __str__(self) -> str:
+        return self.username
+
+    class Meta:
+        ordering = ['-date_joined']
