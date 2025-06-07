9365d8a87b5709bb32435fdc7046d759ab740298
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2025-06-07 20:04:45 +0300

    Solution

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from blog.models import User, Post, Commentary
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + ((("Aditional info"),
+                                                {"fields": (
+                                                    "first_name",
+                                                    "last_name")}),)
+
+
+admin.site.register(Post)
+admin.site.register(Commentary)
+
+admin.site.unregister(Group)
