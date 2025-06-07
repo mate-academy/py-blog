4c7db49959224eb0bcb9265d074c1556654970eb
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-06-07 16:35:16 +0200

    feat: create Post models and register it in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Post
 
 
 # Register your models here.
@@ -10,3 +10,10 @@ class UserAdministrator(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Additional Information', {'fields': ('first_name', 'last_name', 'email')})
     ),
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_time", "owner")
+    list_filter = ("owner", "title", "created_time")
+    search_fields = ("title",)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-# Create your models here.
 
 class User(AbstractUser):
     def __str__(self) -> str:
@@ -10,3 +9,18 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ['-date_joined']
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-created_time"]
+        verbose_name_plural = "Posts"
+        verbose_name = "Post"
