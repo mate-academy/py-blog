22972343b6a833b5c69d658c7d172b58013923e6
Roman Vasilcun <roman.vasilcun@gmail.com>
2025-06-09 17:55:54 +0300

    "Solution"

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Commentary
+
+
+class CommentaryForm(forms.ModelForm):
+    class Meta:
+        model = Commentary
+        fields = ["content"]
+        widgets = {
+            "content": forms.Textarea(attrs={"rows": 4}),
+        }
