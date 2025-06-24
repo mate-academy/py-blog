c21af09170c8a1f00d740a081cc468cf67f59d1d
Vik <viktoria.om@gmail.com>
2025-06-24 18:47:16 +0300

    'Solution2'

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ from .views import PostListView, PostDetailView, CommentaryCreateView
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("commentaty/create/", CommentaryCreateView.as_view(), name="commentary-create"),
+    path("commentaty/create/",
+         CommentaryCreateView.as_view(),
+         name="commentary-create"),
 ]
 
 app_name = "blog"
