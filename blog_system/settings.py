9649d50ae2107a48d181bb28796d404de78c2840
Vik <viktoria.om@gmail.com>
2025-06-24 18:51:44 +0300

    'Solution3'

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
-    "crispy_bootstrap4",
     "blog",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
