201a101de8fa3ab0e6b01bb529ea7bca39ffa618
Maria Hrabovska <mariya76om@gmail.com>
2025-07-11 22:50:15 +0300

    Remove venv from repository

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
