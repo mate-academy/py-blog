3a2279b2fa95f4b1cfe858c382c7cca1364fd647
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-07-14 00:22:55 +0300

    Solution

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog import views
+from blog.views import PostDetailView, PostListView
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='index'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail')
+]
