66c7d84ceff9f553aa16994ebaa64c471c2e3630
MiKPo4eLiK <stassojcuk@gmail.com>
2025-07-28 16:41:45 +0300

    vegkgk

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Comment
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Add Comment"))
