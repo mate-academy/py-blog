ad80432338a8106b9201d787cde3db1ecaf74411
Ruslan Priadko <r.priadko@ukr.net>
2025-07-29 11:42:13 +0300

    solution

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import DetailView, CreateView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from blog.models import Post, Commentary
+from django.conf import settings
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    ordering = ["created_time"]
+    # context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        print(f"DEBUG: TEMPLATES DIRS: {settings.TEMPLATES[0]['DIRS']}")
+        return super().get_queryset().prefetch_related("comments")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comments"] = self.object.comments.all().order_by(
+            "created_time")
+        return context
+
+
+class CommentCreateView(CreateView):
+    model = Commentary
+    fields = "__all__"
+    template_name = "blog/comment-form.html"
+
+    def get_success_url(self):
+        post_pk = self.kwargs.get("pk")
+        return reverse("blog:post-detail", kwargs={"pk": post_pk})
