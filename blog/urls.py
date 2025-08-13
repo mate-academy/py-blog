eaa94daa90abe28231a580bb19c761fc5cb4cfc0
Lev <lev01111989@gmail.com>
2025-08-13 14:33:21 +0200

    My blog

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import IndexView, PostDetailView
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+]
