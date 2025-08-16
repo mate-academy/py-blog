4d2fb92fd0b909213db3bef454114db6d0e7183b
Serhii Basok <sergeybsk19@gmail.com>
2025-08-16 12:14:25 +0300

    Create blog

---
+++ blog/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from blog_system import settings
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=255
+    )
+    content = models.TextField()
+    created_time = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_time = models.DateTimeField(
+        auto_now_add=True
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
