bb58c2a0927381adc15b2add41f2d482231458c0
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-08-16 15:46:44 +0200

    Solution4

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import PostListView, PostDetailView, CommentCreateView
+from blog.views import PostListView, PostDetailView
 
 app_name = "blog"
 
@@ -8,8 +8,4 @@ app_name = "blog"
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/comment/",
-         CommentCreateView.as_view(),
-         name="post-create"
-         ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
-from blog.models import Post, Commentary
+from django.views.generic.edit import FormMixin
+
+from blog.forms import CommentaryForm
+from blog.models import Post
 
 
 class PostListView(generic.ListView):
@@ -15,16 +17,11 @@ class PostListView(generic.ListView):
         return Post.objects.annotate(comment_count=Count("commentaries"))
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(FormMixin, generic.DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
-
-
-class CommentCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Commentary
-    fields = ["content"]
-    template_name = "blog/comment_form.html"
+    form_class = CommentaryForm
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -32,4 +29,21 @@ class CommentCreateView(LoginRequiredMixin, generic.CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse("blog:post-detail", kwargs={"pk": self.object.post.pk})
+        return reverse("blog:post-detail", kwargs={"pk": self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.setdefault("form", self.get_form())
+        ctx["comments"] = self.object.commentaries.all()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = self.object
+            comment.save()
+            return self.form_valid(form)
+        return self.form_invalid(form)
