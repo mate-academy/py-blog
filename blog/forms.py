09d10b5aeedd6dde0f9226676f748e40f2ffb730
Pavlo Semenikhin <pashock1337@gmail.com>
2025-08-18 00:05:39 +0300

    fix flake

--- blog/forms.py
+++ blog/forms.py
@@ -14,4 +14,10 @@ class CommentaryForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
-        self.helper.add_input(Submit("submit", "Add comment", css_class="btn btn-success"))
+        self.helper.add_input(
+            Submit(
+                "submit",
+                "Add comment",
+                css_class="btn btn-success"
+            )
+        )
