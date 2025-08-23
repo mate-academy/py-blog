f265f393e9dd5c76fc4626f7a554e7bf9d1e1619
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-08-23 17:18:06 +0200

    Solution

---
+++ blog/admin.py
@@ -0,0 +1,27 @@
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from blog.models import User, Post, Commentary
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    search_fields = ["username"]
+    list_filter = ["first_name", "last_name", "username"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_filter = ["owner__username", "created_time"]
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    search_fields = ["content"]
+    list_filter = ["user__username", "created_time"]
+
+
+admin.site.unregister(Group)
