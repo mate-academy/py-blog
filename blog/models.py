5ac441418c36d7c7efc606ad96b16c121fa66c59
Tamerlan <egtamerlan@gmail.com>
2025-08-31 22:32:02 +0200

    bugs fixed

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,10 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
