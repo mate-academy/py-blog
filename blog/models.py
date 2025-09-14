d9df512beb2e13525843bfac94122cc4e489c278
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-09-14 14:45:36 +0200

    Add custom User model and Post, Commentary models to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("user_detail", args=[str(self.id)])
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.owner.username})"
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Comment ({self.user.username})"
+
+    def get_absolute_url(self):
+        return reverse("commentary_detail", args=[str(self.id)])
