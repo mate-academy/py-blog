4813cc5a68be49bd9a6b341e06a4232fe15dc288
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-10-08 00:30:32 +0200

    Add related_name to ForeignKey fields in Post and Commentary models for improved query readability

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
@@ -21,12 +21,12 @@ class Post(models.Model):
         return reverse("post-detail", args=[str(self.id)])
 
     def get_comments_count(self):
-        return self.commentary_set.count()
+        return self.comments.count()
 
 
 class Commentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
