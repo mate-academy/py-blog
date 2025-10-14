a945998c4e368c6c9481028b82cd79e1c9dfae43
Alex Plugin <plugin0785@gmail.com>
2025-10-14 03:10:45 +0200

    Solution

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from blog.models import Commentary
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Commentary
+        fields = ["content"]
---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import index, PostDetailView, add_comment
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/comment/", add_comment, name="post-comment"),
+]
