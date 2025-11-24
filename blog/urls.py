483e1f81869cc3ef7c03442d1b51d16d6bc8eb3e
Anastasiia Pohonets <29karasik@gmail.com>
2025-11-24 14:47:15 +0200

    Solution

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from blog.views import PostDetailView, CommentaryCreateView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("comments/create/<int:pk>/",
          CommentaryCreateView.as_view(),
          name="comment-create"),
