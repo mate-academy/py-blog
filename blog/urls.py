336599406bc5e276652ee69bbaac433f0acdd6cb
Anastasiia Pohonets <29karasik@gmail.com>
2025-11-24 14:21:00 +0200

    Solution

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls.conf import path
+
+from blog import views
+from blog.views import PostDetailView, CommentaryCreateView
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("comments/create",
+         CommentaryCreateView.as_view(),
+         name="comment-create"),
+]
